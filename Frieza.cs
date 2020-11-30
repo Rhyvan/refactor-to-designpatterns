f605e0a66912af05a7206d9c8aabfc8e76102502
Rhyvan <polyakan@gmail.com>
2020-11-30 21:18:47 +0100

    :poop: add terribly implemented stateful Goku

---
+++ Frieza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.State
+{
+    public class Frieza
+    {
+        private bool _alive = true;
+
+        public void MakeHimDead()
+        {
+            _alive = false;
+        }
+
+        public bool IsAlive()
+        {
+            return _alive;
+        }
+    }
+}
