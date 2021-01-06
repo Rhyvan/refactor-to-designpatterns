c77e9906926aa553746530abc7dc7dee0cc981c7
Andrea Polyak <54262400+Rhyvan@users.noreply.github.com>
2021-01-06 22:33:31 +0100

    Add strategy exercise (#3)

---
+++ DesignPatterns/BehavioralPatterns/Strategy/Drink.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Strategy
+{
+    public class Drink
+    {
+        public string Name { get; private set; }
+        public Drink(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Tests.DesignPatterns/StrategyTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DesignPatterns.Strategy;
+using NUnit.Framework;
+
+namespace Tests.DesignPatterns
+{
+    [TestFixture]
+    class StrategyTest
+    {
+        VendingMachine machine;
+
+        [SetUp]
+        public void SetUpBeforeEachTest()
+        {
+            machine = new VendingMachine();
+        }
+
+        [Test]
+        public void DefaultBeverageThrowsError()
+        {
+            Assert.Throws<ArgumentException>(() => machine.MakeBeverage(Beverage.Yoghurt));
+        }
+
+        [Test]
+        public void SelectingCoffeResultsCoffe()
+        {
+            var drink = machine.MakeBeverage(Beverage.Espresso);
+            Assert.AreEqual("coffee", drink.Name);
+        }
+
+        [Test]
+        public void MakingCoffeMakesMachineWarm()
+        {
+            var defaultMachineWarmth = machine.IsWarm;
+            machine.MakeBeverage(Beverage.Macchiato);
+
+            Assert.IsFalse(defaultMachineWarmth);
+            Assert.IsTrue(machine.IsWarm);
+        }
+
+     
+        // Do not clutter the VendingMachine with methods that are submethods of MakeBeverage()
+        [Test]
+        public void ObjectHasOnlyOneMethod()
+        {
+            var methodAmountOfType = machine.GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(m => !m.IsSpecialName && m.DeclaringType != typeof(object))
+               .Count();
+
+            Assert.AreEqual(1, methodAmountOfType);
+        }
+    }
+}
